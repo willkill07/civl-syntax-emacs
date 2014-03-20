aae96a30b4f9f3b597d9a520d0bcf2afb67d4592
Will Killian <william.killian@gmail.com>
2014-03-20 09:00:27 -0400

    Added $gcomm_destroy, CIVL header file (.cvh) support; structure improvements

--- civl-syntax.el
+++ civl-syntax.el
@@ -1,5 +1,3 @@
-;;; civl-syntax.el --- CIVL-C Syntax Highlighting
-
 (define-derived-mode civl-mode c-mode
   (setq mode-name "CIVL-C"))
 (add-hook 'civl-mode-hook'
@@ -8,9 +6,12 @@
       '(("\\<_Bool\\>" . font-lock-type-face)
 	("\\$\\(bundle\\|scope\\|proc\\|message\\|gcomm\\|comm\\)\\>" . font-lock-type-face)
 	("\\$\\(here\\|root\\|self\\|true\\|false\\)\\>" . font-lock-constant-face)
-	("\\$\\(scope_parent\\|scopeof\\|choose_int\\|wait\\|exit\\|message_pack\\|message_tag\\|message_dest\\|message_size\\|message_unpack\\|gcomm_create\\|comm_create\\|comm_destroy\\|comm_size\\|comm_place\\|comm_enqueue\\|comm_probe\\|comm_seek\\|comm_dequeue\\|assert\\|malloc\\|free\\)\\>" . font-lock-builtin-face)
+	("\\$\\(scope_parent\\|scopeof\\|choose_int\\|wait\\|exit\\|assert\\|malloc\\|free\\)\\>" . font-lock-builtin-face)
+	("\\$message_\\|pack\\|tag\\|dest\\|size\\|unpack\\)\\>" . font-lock-builtin-face)
+	("\\$gcomm_\\|create\\|destroy\\)\\>" . font-lock-builtin-face)
+	("\\$comm\\|create\\|destroy\\|size\\|place\\|enqueue\\|probe\\|seek\\|dequeue\\)\\>" . font-lock-builtin-face)
 	("\\$\\(when\\|choose\\|spawn\\|atom\\|atomic\\|input\\|output\\|assume\\|forall\\|exists\\|requires\\|ensures\\|invariant\\|collective\\|abstract\\)\\>" . font-lock-keyword-face)))))
 
-(add-to-list 'auto-mode-alist '("\\.cvl\\'" . civl-mode))
+(add-to-list 'auto-mode-alist '("\\.cv\\(l\\|h\\)\\'" . civl-mode))
 
 (provide 'civl-syntax)
