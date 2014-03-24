2f004c0fde062085625c768a6270d2cb332dfb0a
Will Killian <william.killian@gmail.com>
2014-03-24 11:22:35 -0400

    Bug fixes!

--- civl-syntax.el
+++ civl-syntax.el
@@ -1,17 +1,20 @@
+;;; civl-syntax.el --- CIVL-C Syntax Highlighting
+
+;;;###autoload
 (define-derived-mode civl-mode c-mode
   (setq mode-name "CIVL-C"))
-(add-hook 'civl-mode-hook'
+
+;;;###autoload
+(add-hook 'civl-mode-hook
   (lambda ()
     (font-lock-add-keywords nil
       '(("\\<_Bool\\>" . font-lock-type-face)
-	("\\$\\(bundle\\|scope\\|proc\\|message\\|gcomm\\|comm\\)\\>" . font-lock-type-face)
-	("\\$\\(here\\|root\\|self\\|true\\|false\\)\\>" . font-lock-constant-face)
-	("\\$\\(scope_parent\\|scopeof\\|choose_int\\|wait\\|exit\\|assert\\|malloc\\|free\\)\\>" . font-lock-builtin-face)
-	("\\$message_\\|pack\\|tag\\|dest\\|size\\|unpack\\)\\>" . font-lock-builtin-face)
-	("\\$gcomm_\\|create\\|destroy\\)\\>" . font-lock-builtin-face)
-	("\\$comm\\|create\\|destroy\\|size\\|place\\|enqueue\\|probe\\|seek\\|dequeue\\)\\>" . font-lock-builtin-face)
-	("\\$\\(when\\|choose\\|spawn\\|atom\\|atomic\\|input\\|output\\|assume\\|forall\\|exists\\|requires\\|ensures\\|invariant\\|collective\\|abstract\\)\\>" . font-lock-keyword-face)))))
+    	("\\$\\(bundle\\|scope\\|proc\\|message\\|gcomm\\|comm\\)\\>" . font-lock-type-face)
+    	("\\$\\(here\\|root\\|self\\|true\\|false\\)\\>" . font-lock-constant-face)
+    	("\\$\\(scope_parent\\|scopeof\\|choose_int\\|wait\\|exit\\|message_pack\\|message_tag\\|message_dest\\|message_size\\|message_unpack\\|gcomm_create\\|gcomm_destroy\\|comm_destroy\\|comm_create\\|comm_size\\|comm_place\\|comm_enqueue\\|comm_probe\\|comm_seek\\|comm_dequeue\\|assert\\|malloc\\|free\\)\\>" . font-lock-builtin-face)
+    	("\\$\\(when\\|choose\\|spawn\\|atom\\|atomic\\|input\\|output\\|assume\\|forall\\|exists\\|requires\\|ensures\\|invariant\\|collective\\|abstract\\)\\>" . font-lock-keyword-face)))))
 
-(add-to-list 'auto-mode-alist '("\\.cv\\(l\\|h\\)\\'" . civl-mode))
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.\\(cvl\\|cvh\\)" . civl-mode))
 
 (provide 'civl-syntax)
